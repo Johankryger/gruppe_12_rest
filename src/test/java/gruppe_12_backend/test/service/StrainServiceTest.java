1a2056aa625b81798594d873a3f4d9b4b89bc7bf
Mohammad Uddin <s184174@student.dtu.dk>
2022-10-20 17:40:54 +0200

    Added test class

---
+++ src/test/java/gruppe_12_backend/test/service/StrainServiceTest.java
@@ -0,0 +1,16 @@
+package gruppe_12_backend.test.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StrainServiceTest {
+
+    @Test
+    void getStrain() {
+    }
+
+    @Test
+    void createStrain() {
+    }
+}
