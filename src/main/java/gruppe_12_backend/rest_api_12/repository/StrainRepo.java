33de1e124711b149d89e73fb296125a9bc4f964a
anthai87 <Hashai87>
2022-11-09 17:43:56 +0100

    changed the type of ID from int to Long.

--- src/main/java/gruppe_12_backend/rest_api_12/repository/StrainRepo.java
+++ src/main/java/gruppe_12_backend/rest_api_12/repository/StrainRepo.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StrainRepo extends CrudRepository<Strain,Integer> {
+public interface StrainRepo extends CrudRepository<Strain,Long> {
     Strain findById(long id);
 }
