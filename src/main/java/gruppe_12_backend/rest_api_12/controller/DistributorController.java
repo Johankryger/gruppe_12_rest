b519b5b95875e0cd8a9c6a9f8f05ca8e7cafc740
Mohammad Uddin <s184174@student.dtu.dk>
2022-11-26 11:15:05 +0100

    Create and delete distributor service methods and test

--- src/main/java/gruppe_12_backend/rest_api_12/controller/DistributorController.java
+++ src/main/java/gruppe_12_backend/rest_api_12/controller/DistributorController.java
@@ -5,9 +5,9 @@ import gruppe_12_backend.rest_api_12.service.DistributorService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Api(tags = "Distributor")
 @ResponseBody
@@ -21,13 +21,13 @@ public class DistributorController {
 //    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Get a distributor by id", notes = "Returns a distributor given the specific id")
     @GetMapping("/{id}")
-    public Distributor getDistributor(@PathVariable("id") long id){
+    public Distributor getDistributor(@PathVariable("id") Long id){
         return distributorService.getDistributor(id);
     }
 
     @ApiOperation(value = "Get a distributor list", notes = "Returns a distributor given the specific id")
     @GetMapping
-    public Iterable<Distributor> getAllDistributors(){
+    public List<Distributor> getAllDistributors(){
         return distributorService.getAllDistributors();
     }
 
--- src/main/java/gruppe_12_backend/rest_api_12/service/DistributorService.java
+++ src/main/java/gruppe_12_backend/rest_api_12/service/DistributorService.java
@@ -3,27 +3,44 @@ package gruppe_12_backend.rest_api_12.service;
 import gruppe_12_backend.rest_api_12.model.Distributor;
 import gruppe_12_backend.rest_api_12.repository.DistributorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.geo.Point;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class DistributorService {
-
     @Autowired
     private DistributorRepository distributorRepository;
-
-    public DistributorService(){
-    }
 
     public Distributor getDistributor(long id) {
         return distributorRepository.findById(id);
     }
 
-    public Iterable<Distributor> getAllDistributors() {
-        return distributorRepository.findAll();
+    public List<Distributor> getAllDistributors() {
+        List<Distributor> distributors = new ArrayList<>();
+        distributorRepository.findAll().iterator().forEachRemaining(distributors::add);
+        return distributors;
+    }
+
+    public long createDistributor(Distributor distributor) {
+        List<Distributor> all = getAllDistributors();
+        distributor.setId(all.get(all.size() - 1).getId() + 1L);
+
+        distributorRepository.save(distributor);
+
+        return distributor.getId();
+    }
+
+    public void deleteDistributor (Distributor distributor) {
+        Optional<Distributor> existingDistributor = distributorRepository.findById(distributor.getId());
+
+        if (existingDistributor.isPresent()) {
+            distributorRepository.delete(distributor);
+        }
     }
 }
